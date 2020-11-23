7f9bd2ddc76c123bdb97724dab1c022cfb5784ef
Stephen Wanhella <stephen.wanhella@gmail.com>
2020-11-23 14:01:45 -0800

    add more game logic

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const battleship = document.querySelector('.battleship-container');
     const carrier = document.querySelector('.carrier-container');
     const startButton = document.querySelector('#start');
-    const rotateButton = document.querySelector('#rotate')
+    const rotateButton = document.querySelector('#rotate');
     const turnDisplay = document.querySelector('#whose-turn');
     const infoDisplay = document.querySelector('#info');
     const userSquares = [];
@@ -42,32 +42,31 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'submarine',
             directions: [
                 [0, 1, 2],
-                [0, width, width*2]
+                [0, width, width * 2]
             ]
         },
         {
             name: 'cruiser',
             directions: [
                 [0, 1, 2],
-                [0, width, width*2]
+                [0, width, width * 2]
             ]
         },
         {
             name: 'battleship',
             directions: [
                 [0, 1, 2, 3],
-                [0, width, width*2, width*3]
+                [0, width, width * 2, width * 3]
             ]
         },
         {
             name: 'carrier',
             directions: [
                 [0, 1, 2, 3, 4],
-                [0, width, width*2, width*3, width*4]
+                [0, width, width * 2, width * 3, width * 4]
             ]
         }
     ];
-
 
     function generate(ship) {
         let randomDirection = Math.floor(Math.random() * ship.directions.length);
@@ -101,6 +100,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     rotateButton.addEventListener('click', rotate);
 
+    //move around user ship
     ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
     userSquares.forEach(square => square.addEventListener('dragstart', dragStart))
     userSquares.forEach(square => square.addEventListener('dragover', dragOver))
@@ -109,23 +109,23 @@ document.addEventListener('DOMContentLoaded', () => {
     userSquares.forEach(square => square.addEventListener('drop', dragDrop))
     userSquares.forEach(square => square.addEventListener('dragend', dragEnd))
 
-    let selectedShipNameWithIndex
-    let draggedShip
-    let draggedShipLength
+    let selectedShipNameWithIndex;
+    let draggedShip;
+    let draggedShipLength;
 
     ships.forEach(ship => ship.addEventListener('mousedown', e => {
         selectedShipNameWithIndex = e.target.id;
         console.log(selectedShipNameWithIndex);
     }));
 
-    function dragStart(e) {
+    function dragStart() {
         draggedShip = this;
         draggedShipLength = this.children.length;
-        console.log(draggedShipLength)
+        console.log(draggedShip);
     }
 
     function dragOver(e) {
-        e.preventDefault()
+        e.preventDefault();
     }
 
     function dragEnter(e) {
@@ -133,36 +133,43 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function dragLeave() {
-
+        console.log('drag leave');
     }
 
     function dragDrop() {
         let shipNameWithLastId = draggedShip.lastElementChild.id;
         let shipClass = shipNameWithLastId.slice(0, -2);
+        console.log(shipClass);
         let lastShipIndex = parseInt(shipNameWithLastId.substr(-1));
         let shipLastId = lastShipIndex + parseInt(this.dataset.id);
-        
+        console.log(shipLastId);
+        const notAllowedHorizontal = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 2, 22, 32, 42, 52, 62, 72, 82, 92, 3, 13, 23, 33, 43, 53, 63, 73, 83, 93];
+        const notAllowedVertical = [99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60];
+
+        let newNotAllowedHorizontal = notAllowedHorizontal.splice(0, 10 * lastShipIndex);
+        let newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lastShipIndex);
+
         selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1));
 
         shipLastId = shipLastId - selectedShipIndex;
         console.log(shipLastId);
-        const notAllowedHorizontal = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 2, 22, 32, 42, 52, 62, 72, 82, 92, 3, 13, 23, 33, 43, 53, 63, 73, 83, 93]
-        const notAllowedVertical = [99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60]
 
-        if (isHorizontal) {
+        if (isHorizontal && !newNotAllowedHorizontal.includes(shipLastId)) {
             for (let i = 0; i < draggedShipLength; i++) {
-                userSquares[parseInt(this.dataset.id) + i].classList.add('taken', shipClass);
+                userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass);
             }
-        } else if (!isHorizontal) {
+            //As long as the index of the ship you are dragging is not in the newNotAllowedVertical array! This means that sometimes if you drag the ship by its
+            //index-1 , index-2 and so on, the ship will rebound back to the displayGrid.
+        } else if (!isHorizontal && !newNotAllowedVertical.includes(shipLastId)) {
             for (let i = 0; i < draggedShipLength; i++) {
-                userSquares[parseInt(this.dataset.id) - selectedShipIndex + width*i].classList.add('taken', shipClass);
+                userSquares[parseInt(this.dataset.id) - selectedShipIndex + width * i].classList.add('taken', shipClass);
             }
-        }
+        } else return;
 
         displayGrid.removeChild(draggedShip);
     }
 
     function dragEnd() {
-
+        console.log('dragend');
     }
 });
